a4c2bc213b9eaaac039ae5aff95297debc2de6ea
Shawn Rider <shawn@shawnrider.com>
2015-06-13 12:37:18 -0700

    Added event code for social image download.
    Change-Id: I797d9fba802fce8282bc5b9e0c60994e5add9da6

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
 // scripts go here
+$(document).on('ready', function(){
+    $('.social-poster').on('click', function(){
+        ga('send', {
+          'hitType': 'event',          // Required.
+          'eventCategory': 'social',   // Required.
+          'eventAction': 'download',      // Required.
+          'eventLabel': 'shareables',
+          'eventValue': 1
+        });
+    })
+});
